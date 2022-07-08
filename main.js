f5168406d1041f809f30fe4eedd6cd6998760d08
DiepVanTy <102716047+DiepVanTy@users.noreply.github.com>
2022-07-08 09:44:20 +0700

    nine commit

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 $(document).ready(function () {
+
+  // menu for mobile
+
   $(".menu__icon").click(function () {
     $(this).toggleClass("active");
-    $('.navigation').toggleClass("active");
+    $(".navigation").toggleClass("active");
+  });
+
+  // search 
+
+  $(".btn--search").click(function () {
+    $(".search-bar, .page-overlay").css("display", "block");
+    $(".search-bar").focus();
+  });
+  $(".search-bar__close").mousedown(function () {
+    $(".search-bar, .page-overlay").css("display", "none");
   });
 });
+
+// choose products to be sold or rented
+
+function productListSelection(elm, classOfList) {
+  let btn = document.querySelectorAll(".two-btn .btn");
+  let listProduct = document.querySelectorAll(".product .product__list");
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].classList.remove("active");
+    listProduct[i].classList.remove("is-show");
+  }
+  elm.classList.add("active");
+  document.querySelector(classOfList).classList.add("is-show");
+}
+function scrollShow () {
+  console.log(window.scrollY)
+    let elements = document.querySelector('.scrollshow');
+    console.log(elements.scrollTop)
+    if(window.scrollY == elements.scrollTop) {
+        console.log('oke')
+    }
+}
